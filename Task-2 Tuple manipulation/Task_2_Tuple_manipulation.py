2d795f13242ecd48fde948b51d23db708db26683
Saharkhan107 <iqbalsahar121@gmail.com>
2024-09-20 22:02:19 -0600

    Add project files.

---
+++ Task-2 Tuple manipulation/Task_2_Tuple_manipulation.py
@@ -0,0 +1,17 @@
+#Course: CIS 103
+#Instructor: Md Ali
+#Student: Sahar Iqbal
+#Date: 09/20/2024
+
+# Task: 02 Tuple Manipulation:
+
+# Tuple representing the stats of a video game character:
+character_stats = (100, 50, 30)  # (health, attack, defense)
+print("Original Character Stats:", character_stats)
+
+# Create a new tuple with upgraded stats
+# Increase attack by 10 and defense by 5
+upgraded_stats = (character_stats[0], character_stats[1] + 10, character_stats[2] + 5)
+
+# Print both the original and upgraded stats
+print("Upgraded Character Stats:", upgraded_stats) 
